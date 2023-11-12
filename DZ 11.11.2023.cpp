a369f2eb4004f4e53ea27dc63b886c3ee71eb14f
55day <147132026+55day@users.noreply.github.com>
2023-11-13 00:48:47 +0300

    Add files via upload

---
+++ DZ 11.11.2023.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+
+    int dano;
+    int posled = 1;    
+
+    string start = "0";        // Начальное число
+    cout << "[+] Введите ограничение:";
+    cin >> dano;                // Кол-во действий
+    system("cls");
+    for (int i = 0;i < dano;i++) {   
+        string numpl = start;              // Назначение кол-во символа 
+        for (int j = 0;j < start.size();j++) {
+            if (start[j] == '0') {
+                numpl[j] = '1';
+            }
+            if (start[j] == '1') {
+                numpl[j] = '0';
+            }
+        }
+        start = start+numpl;  // Прибавление чаин к плюсу
+        cout << "Последовательность числа " << posled++ << endl;
+        cout << start << endl << endl;;
+    }
+
+    return 0;
+}
+// 0
